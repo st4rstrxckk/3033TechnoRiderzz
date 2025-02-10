2bd5b246f13c8f2795f5bce6e5a67cbb6a44e7c0
st4rstrxckk <squigdrawz@gmail.com>
2025-02-10 10:58:39 -0600

    i might be stupid

--- TeamCode/src/main/java/COMPCODE/TECHMAP.java
+++ TeamCode/src/main/java/COMPCODE/TECHMAP.java
@@ -1,9 +1,97 @@
 package COMPCODE;
 
-//TODO: HARDWARE MAP PLEASEEE
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.hardware.DcMotor; //DcMotorEx = expansion hub omg
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+//TODO: DOUBLE CHECK HARDWARE MAP PLEASEEE
+@Config
 public class TECHMAP {
 
+    public DcMotor leftFront = null;
+    public DcMotor rightFront = null;
+    public DcMotor leftBack = null;
+    public DcMotor rightBack = null;
+    public DcMotor lift = null;
+    public DcMotor intake = null;
+    public DcMotor hang = null;
+    public DcMotor hang2 = null;
+
+    public Servo wristL = null;
+    public Servo wristR = null;
+    public Servo bucket = null;
+    public Servo horiz = null;
+  //  public Servo turn = null;
+
+    HardwareMap hwMap  =  null;
+    private ElapsedTime period  = new ElapsedTime();
+
+    /* Constructor */
+    public TECHMAP(){
+
+    }
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+        // Save reference to Hardware map
+        hwMap = ahwMap;
+
+        // TODO: Define and Initialize Motors
+        leftFront  = hwMap.get(DcMotor.class, "frontL"); //drive stuff is in the expansion hub
+        rightFront = hwMap.get(DcMotor.class, "frontR");
+        leftBack  = hwMap.get(DcMotor.class, "backL");
+        rightBack = hwMap.get(DcMotor.class, "backR");
+
+        wristL = hwMap.get(Servo.class, "wristL");
+        wristR = hwMap.get(Servo.class, "wristR");
+        bucket = hwMap.get(Servo.class, "bucket");
+        horiz = hwMap.get(Servo.class, "horiz");
+      //  turn = hwMap.get(Servo.class, "RE");
+
+        leftFront.setDirection(DcMotor.Direction.REVERSE);
+        rightFront.setDirection(DcMotor.Direction.REVERSE);
+        leftBack.setDirection(DcMotor.Direction.REVERSE);
+        rightBack.setDirection(DcMotor.Direction.REVERSE);
+
+        hang.setDirection(DcMotor.Direction.REVERSE);
+        hang2.setDirection(DcMotor.Direction.FORWARD);
+        intake.setDirection(DcMotor.Direction.REVERSE);
+
+        horiz.setDirection(Servo.Direction.REVERSE);
+        bucket.setDirection(Servo.Direction.REVERSE);
+        wristL.setDirection(Servo.Direction.REVERSE);
+        wristR.setDirection(Servo.Direction.FORWARD);
+
+        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
 
+        leftFront.setPower(0);
+        rightFront.setPower(0);
+        leftBack.setPower(0);
+        rightBack.setPower(0);
 
+//        lin.setPower(0);
+//        rin.setPower(0);
+
+
+        // Set all motors to run without encoders.
+        // May want to use RUN_USING_ENCODERS if encoders are installed.
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//
+
+        // Define and initialize ALL installed servos.
+
+
+    }
 }
---
+++ TeamCode/src/main/java/COMPCODE/trafficLaws.java
@@ -0,0 +1,30 @@
+package COMPCODE;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+//import com.arcrobotics.ftclib.controller.PIDController;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import COMPCODE.TECHMAP;
+
+@TeleOp(name="kimy cannot drive")
+public class trafficLaws extends LinearOpMode {
+TECHMAP robot = new TECHMAP();
+    ElapsedTime toggleTimer = new ElapsedTime();
+
+
+
+
+
+
+
+}
